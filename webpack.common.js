d417bf61f3a6a927b8c4891573ced794b529b0ea
Haroun Ach <harounacho21@gmail.com>
2021-11-20 22:24:13 +0100

    Set up sass

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
